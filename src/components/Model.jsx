45a3d4ed46a794e8ae584fc7447166569995e4e8
Kae <kaehehehe@gmail.com>
2025-02-03 23:42:14 +0900

    refactor: remove strawberry-pink.glb

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Model() {
-  const { scene: red } = useGLTF("strawberry.glb");
-  const { scene: pink } = useGLTF("strawberry-pink.glb");
+  const { scene } = useGLTF("strawberry.glb");
 
   return (
     <>
@@ -20,7 +19,21 @@ export function Model() {
           Math.random() * Math.PI,
         ];
 
-        const model = Math.random() < 0.5 ? red : pink;
+        const isPink = Math.random() < 0.5;
+        const model = scene.clone();
+
+        if (isPink) {
+          model.traverse(({ children }) => {
+            if (children.length === 0) return;
+
+            const [stem, fruit] = children;
+            stem.material = stem.material.clone();
+            stem.material.color.set("#6fa54c");
+
+            fruit.material = fruit.material.clone();
+            fruit.material.color.set("#e4748a");
+          });
+        }
 
         return (
           <mesh
@@ -29,7 +42,7 @@ export function Model() {
             rotation={rotation}
             scale={[500, 500, 500]}
           >
-            <primitive object={model.clone()} />
+            <primitive object={model} />
           </mesh>
         );
       })}
