36ab68b79262378f0513dedfc57916580464249a
KaeOkamura <kae.okamura@partridgesystems.com>
2025-02-04 13:05:43 +0900

    feat: update strawberry field

--- src/components/StrawberryField.jsx
+++ src/components/StrawberryField.jsx
@@ -3,17 +3,17 @@ import { CreateStrawberry } from "./CreateStrawberry";
 
 export function StrawberryField() {
   const strawberries = [
-    ...Array.from({ length: 350 }, () => "red"),
-    ...Array.from({ length: 150 }, () => "pink"),
+    ...Array.from({ length: 900 }, () => "red"),
+    ...Array.from({ length: 300 }, () => "pink"),
   ];
 
   return (
     <>
       {strawberries.map((color, i) => {
         const position = [
-          8000 * (2.0 * Math.random() - 1.0),
-          8000 * (2.0 * Math.random() - 1.0),
-          8000 * (2.0 * Math.random() - 1.0),
+          8000 * (2.0 * Math.random() - 1.0), // -8000 ~ 8000
+          8000 * (2.0 * Math.random() - 1.0), // -8000 ~ 8000
+          8000 * (2.0 * Math.random() - 1.0), // -8000 ~ 8000
         ];
 
         const rotation = [
